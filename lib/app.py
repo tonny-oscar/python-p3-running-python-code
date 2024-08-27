178355c025514d14d057b5a5d6925977c9324c1f
Tonny Bett <betttonny26@gmail.com>
2024-08-27 10:33:22 +0300

    complete python p3 running python code

---
+++ lib/app.py
@@ -0,0 +1,26 @@
+# lib/app.py
+print("Hello world!")
+
+# lib/app.py
+print("Hello world!")
+print("Hello sun!")
+print("Hello sky!")
+
+# lib/app.py
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
+
+# Python3
+dog_name = "Lucy"
+print(f"Say hello to my dog {dog_name}")
+
+
+print ("hello".capitalize())
+
+#numbers
+print(int(10.0))
+
+#dictionary
+print(dict(a=1, b=2))
+
